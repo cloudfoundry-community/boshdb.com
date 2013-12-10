4510cbbbe069e7ff40ea3b80f1396b402133fdc7
Brian McClain <brianmmcclain@gmail.com>
2013-12-09 21:58:28 -0500

    Initial rails 4 scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BoshdbCom::Application.config.secret_key_base = '02d54ab08db24c5e09d32144da4e673c8914c6c20007526c80b47e2d963de884eff8daf462f1edafe14e9d3c7d4e0ab966ddaf335039230f38e058f7a8015c99'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BoshdbCom::Application.config.session_store :cookie_store, key: '_boshdb_com_session'
